b82f27e970e9e8fffe046433f6fc85de8801c49e
Estevao <estevaopbsantos@gmail.com>
2021-10-02 19:17:36 -0300

    testmp

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ def test_Al10_mp():
     crossover_elitism = lambda x: 1
     fitness_param = '!RKS STATE 1.1 Energy'
 
-    result, strategies = optimize(molecule, fitness_param, strategies, max_age, pool_size, max_seconds, time_tolerance,
+    result, strategies = optimize(molecule, fitness_param, strategies, max_age, pool_size, None, None,
     crossover_elitism, mutate_after_crossover=True, parallelism=True, elit_size=2, elitism_rate=[2, 2],
-    generations_tolerance=200, threads_per_calculation=2, max_gens=10000, mutation_rate=2)
+    generations_tolerance=None, threads_per_calculation=2, max_gens=None, mutation_rate=2)
     result.save('best_Al10_mp', directory='')
     return result, strategies
 
